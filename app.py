e8f9335aa7cc1509e55e38b037ee36ee1a8d48e2
Shoeb Shaikh <90shoeb@gmail.com>
2023-10-08 23:44:42 +0800

    adding all files

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
 import streamlit as st
+import pandas as pd
+from huggingface_hub import InferenceClient
+client = InferenceClient(token=st.secrets['hf_key'],model=st.secrets['model'])
+
+st.subheader('Failure Mode Classification')
+
+st.sidebar.info('Training Data')
+uploaded_file = st.sidebar.file_uploader("Please upload a csv file containing failure description and \
+                                         failure codes. See example csv file below.",type=['csv'])
+sample_train = pd.read_csv('train.csv')
+st.sidebar.dataframe(sample_train, hide_index=True)
+
+if uploaded_file is not None:
+    train_csv = pd.read_csv(uploaded_file)
+    st.dataframe(train_csv.head(10))
+    failure_text = st.text_input(label='Please enter failure description', value='')
+
+    if st.button('Get Failure Code'):       
+        
+        newline = '\n'
+        prompt_header = f"""
+        Your answer should contain only the failure mode and nothing else. Valid failure modes are:
+
+        {(newline).join(list(train_csv.iloc[:,1].unique()))}
+
+        """
+        in_context_learning = []
+        for i,row in train_csv.iterrows():
+            in_context_row = f"""
+            Determine the failure mode associated with the following sentence:  
+            sentence: {row.iloc[0]}
+            Response: {row.iloc[1]}
+            """
+            in_context_learning.append(in_context_row)
+            
+        prompt = prompt_header + newline + newline.join(in_context_learning)
+        response = client.text_generation(prompt=prompt,max_new_tokens=5)
+        c1,_,_ = st.columns(3)
+        with c1:
+            st.info(response)
